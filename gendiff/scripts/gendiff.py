df5c0331cd6576f407e391ac5a79db1eda425511
HardDuck69 <tmu1408@gmail.com>
2023-02-17 22:40:39 +0300

    step2: upd gendiff script

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -2,30 +2,18 @@
 
 import argparse
 
-
 def main():
-    parser = argparse.ArgumentParser(
-        description='Compares two configuration files and shows a difference.'
-    )
-
-    parser.add_argument(
-        'first_file',
-        help='path to the first file'
-    )
-
-    parser.add_argument(
-        'second_file',
-        help='path to the second file'
-    )
-
-    parser.add_argument(
-        '-v', '--version',
-        action='version',
-        version='%(prog)s 1.0'
-    )
-
+    parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
+    parser.add_argument('first_file', help='first file to compare')
+    parser.add_argument('second_file', help='second file to compare')
+    parser.add_argument('-f', '--format', help='set format of output', default='stylish')
+    parser.add_argument('-v', '--version', action='version', version='%(prog)s 1.0')
     args = parser.parse_args()
 
+    if args.format:
+        print(f'Comparing files: {args.first_file} vs {args.second_file} using format: {args.format}')
+    else:
+        print(f'Comparing files: {args.first_file} vs {args.second_file}')
 
 if __name__ == '__main__':
     main()
