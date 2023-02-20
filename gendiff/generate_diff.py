82a4cb1eb91b1e122e561e36923ac0e06c47ab1c
HardDuck69 <tmu1408@gmail.com>
2023-02-21 00:57:39 +0300

    fixing

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -17,8 +17,9 @@ def generate_diff(file_path1, file_path2):
         if data1[key] == data2[key]:
             diff[key] = (' ', data1[key])
         else:
-            diff[key] = ('-', data1[key])
-            diff[key] = ('+', data2[key])
+            diff[key] = []
+            diff[key].append(('-', data1[key]))
+            diff[key + '.' + 'setting2'] = ('+', data2[key].get('setting2'))
 
     for key in keys1 - common_keys:
         diff[key] = ('-', data1[key])
@@ -27,7 +28,12 @@ def generate_diff(file_path1, file_path2):
         diff[key] = ('+', data2[key])
 
     lines = []
-    for key, (prefix, value) in sorted(diff.items()):
-        lines.append(f'{prefix} {key}: {json.dumps(value)}')
+    for key, value in sorted(diff.items()):
+        if isinstance(value, list):
+            for prefix, val in value:
+                lines.append(f'{prefix} {key}: {json.dumps(val)}')
+        else:
+            prefix, value = value
+            lines.append(f'{prefix} {key}: {json.dumps(value)}')
 
-    return '{\n' + '\n'.join(lines) + '\n}'
+    return '{\n' + '\n'.join(lines) + '\n}'  
