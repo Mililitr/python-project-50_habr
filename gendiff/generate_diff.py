ef7dd42f681f116ecbbb70218484ed50c87f1f44
HardDuck69 <tmu1408@gmail.com>
2023-02-21 01:25:33 +0300

    upd

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -2,38 +2,23 @@ import json
 
 
 def generate_diff(file_path1, file_path2):
-    with open(file_path1) as file1:
-        data1 = json.load(file1)
+    with open(file_path1) as f1, open(file_path2) as f2:
+        file1 = json.load(f1)
+        file2 = json.load(f2)
 
-    with open(file_path2) as file2:
-        data2 = json.load(file2)
+    keys = sorted(file1.keys() | file2.keys())
 
-    diff = {}
-    keys1 = set(data1.keys())
-    keys2 = set(data2.keys())
-    common_keys = keys1.intersection(keys2)
+    diff = dict()
 
-    for key in common_keys:
-        if data1[key] == data2[key]:
-            diff[key] = (' ', data1[key])
+    for key in keys:
+        if key in file1 and key in file2:
+            if file1[key] == file2[key]:
+                diff[key] = {'value': file1[key], 'type': 'unchanged'}
+            else:
+                diff[key] = {'value1': file1[key], 'value2': file2[key], 'type': 'changed'}
+        elif key in file1:
+            diff[key] = {'value': file1[key], 'type': 'removed'}
         else:
-            diff[key] = []
-            diff[key].append(('-', data1[key]))
-            diff[key + '.' + 'setting2'] = ('+', data2[key].get('setting2'))
+            diff[key] = {'value': file2[key], 'type': 'added'}
 
-    for key in keys1 - common_keys:
-        diff[key] = ('-', data1[key])
-
-    for key in keys2 - common_keys:
-        diff[key] = ('+', data2[key])
-
-    lines = []
-    for key, value in sorted(diff.items()):
-        if isinstance(value, list):
-            for prefix, val in value:
-                lines.append(f'{prefix} {key}: {json.dumps(val)}')
-        else:
-            prefix, value = value
-            lines.append(f'{prefix} {key}: {json.dumps(value)}')
-
-    return '{\n' + '\n'.join(lines) + '\n}'  
+    return json.dumps(diff, indent=4)
--- tests/comparing_test.py
+++ tests/comparing_test.py
@@ -1,54 +1,25 @@
-import json
-import unittest
-from gendiff.cli import generate_diff
+import os
+import pytest
+from gendiff.generate_diff import generate_diff
 
-class TestGenerateDiff(unittest.TestCase):
-    def test_generate_diff_plain_files(self):
-        # Проверяем, что функция правильно сравнивает два плоских файла
-        file1_path = 'tests/fixtures/file1.json'
-        file2_path = 'tests/fixtures/file2.json'
-        expected_output = '''{
-- follow: false
-  host: "hexlet.io"
-- proxy: "123.234.53.22"
-+ timeout: 50
-+ verbose: true
-}'''
-        self.assertMultiLineEqual(generate_diff(file1_path, file2_path), expected_output)
 
-    def test_generate_diff_nested_files(self):
-        # Проверяем, что функция правильно сравнивает два вложенных файла
-        file1_path = 'tests/fixtures/file3.json'
-        file2_path = 'tests/fixtures/file4.json'
-        expected_output = '''{
-    common: {
-        setting1: "Value 1"
--       setting2: 200
-+       setting3: {
-+           key: value
-+       }
-+       setting4: "value4"
-    }
--   group1: {
--       abc: 12345
--       def: {
--           ghi: 67890
--       }
--   }
-+   group2: {
-+       abc: 54321
-+       def: {
-+           ghi: 9876
-+       }
-+       group3: {
-+           key1: value1
-+           key2: {
-+               key3: value3
-+           }
-+       }
-+   }
-}'''
-        self.assertMultiLineEqual(generate_diff(file1_path, file2_path), expected_output)
+FIXTURES_PATH = os.path.join(os.path.dirname(__file__), 'fixtures')
 
-if __name__ == "__main__":
-    unittest.main()
+
+def read_fixture(file_name):
+    with open(os.path.join(FIXTURES_PATH, file_name)) as f:
+        return f.read()
+
+
+def test_generate_diff_plain_files():
+    file1 = 'file1.json'
+    file2 = 'file2.json'
+    expected_output = read_fixture('expected_plain_diff.txt').strip()
+    assert generate_diff(os.path.join(FIXTURES_PATH, file1), os.path.join(FIXTURES_PATH, file2)) == expected_output
+
+
+def test_generate_diff_nested_files():
+    file1 = 'file1.json'
+    file2 = 'file2.json'
+    expected_output = read_fixture('expected_nested_diff.txt').strip()
+    assert generate_diff(os.path.join(FIXTURES_PATH, file1), os.path.join(FIXTURES_PATH, file2)) == expected_output
