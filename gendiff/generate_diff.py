937d6551b898ac30e51af624ccb24b46f9121296
HardDuck69 <tmu1408@gmail.com>
2023-03-27 01:15:25 +0300

    fixing

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -9,32 +9,31 @@ from gendiff.parser import parse
 
 def make_diff(data1, data2, parent=None):
     """Сравнивает два словаря и формирует результат.
-
     Args:
         data1: первый словарь.
         data2: второй словарь.
         parent: родительский ключ.
-
     Returns:
         Результат сравнения в виде словаря.
     """
     if parent is None:
         parent = []
-    diff = []
-    for key in sorted(data1.keys() | data2.keys()):
+    diff = {}
+    for key in data1.keys() | data2.keys():
         path = [*parent, key]
         path_str = '.'.join(path)
         if key not in data1:
-            diff.append({'key': path_str, 'type': 'added', 'value': data2[key]})
+            diff[path_str] = ("added", data2[key])
         elif key not in data2:
-            diff.append({'key': path_str, 'type': 'deleted', 'value': data1[key]})
+            diff[path_str] = ("deleted", data1[key])
         elif data1[key] == data2[key]:
-            diff.append({'key': path_str, 'type': 'unchanged', 'value': data1[key]})
-        elif isinstance(data1[key], dict) and isinstance(data2[key], dict):
-            diff.append({'key': path_str, 'type': 'nested', 'children': make_diff(data1[key], data2[key], path)})
+            diff[path_str] = ("unchanged", data1[key])
+        elif isinstance(data1[key], (dict, list)) and isinstance(data2[key], (dict, list)):
+            diff.update(make_diff(data1[key], data2[key], path))
         else:
-            diff.append({'key': path_str, 'type': 'updated', 'old_value': data1[key], 'new_value': data2[key]})
+            diff[path_str] = ("updated", (data1[key], data2[key]))
     return diff
+
 
 def generate_diff(filepath1, filepath2, output_format='stylish'):
     filepath1_abs = os.path.abspath(filepath1)
