74bde7f62c0a5432ffb18e67550132cf932ecbc9
HardDuck69 <tmu1408@gmail.com>
2023-04-14 17:45:04 +0300

    upd stylish formatter

--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -22,22 +22,22 @@ def format_dict(diff, depth=0):
     for key, value in diff.items():
         if isinstance(value, dict):
             value = format_dict(value, depth + 1)
-            lines.append(f"{SPACES * (depth + 2)}{key.strip()}: {value}")
+            lines.append(f"{SPACES * (depth + 1)}{key.strip()}: {value}")
         elif value == "added":
             value = format_value(diff[key][1], depth + 1)
-            lines.append(f"{ADDED}{SPACES * (depth + 2)}{key.strip()}: {value}")
+            lines.append(f"{ADDED}{SPACES * (depth + 1)}{key.strip()}: {value}")
         elif value == "deleted":
             value = format_value(diff[key][0], depth + 1)
-            lines.append(f"{REMOVED}{SPACES * (depth + 2)}{key.strip()}: {value}")
+            lines.append(f"{REMOVED}{SPACES * (depth + 1)}{key.strip()}: {value}")
         elif isinstance(value, tuple) and len(value) == 2:
             old_value, new_value = value
             old_value = format_value(old_value, depth + 1)
             new_value = format_value(new_value, depth + 1)
-            lines.append(f"{REMOVED}{SPACES * (depth + 2)}{key.strip()}: {old_value}")
-            lines.append(f"{ADDED}{SPACES * (depth + 2)}{key.strip()}: {new_value}")
+            lines.append(f"{REMOVED}{SPACES * (depth + 1)}{key.strip()}: {old_value}")
+            lines.append(f"{ADDED}{SPACES * (depth + 1)}{key.strip()}: {new_value}")
         else:
             value = format_value(value, depth + 1)
-            lines.append(f"{SPACES * (depth + 2)}{key.strip()}: {value}")
+            lines.append(f"{SPACES * (depth + 1)}{key.strip()}: {value}")
 
     return "\n".join(lines)
 
