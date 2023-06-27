c30c336fef66ac19e26749cdcae9d9a5880c1045
HardDuck69 <tmu1408@gmail.com>
2023-06-28 01:24:11 +0300

    bla

--- gendiff/generat_diff.py
+++ gendiff/generat_diff.py
@@ -1,50 +1,71 @@
-import os
+import json
+import yaml
+from pathlib import Path
 from gendiff.formatters.stylish import format_diff_as_stylish
 from gendiff.formatters.plain import format_diff_as_plain
 from gendiff.formatters.as_json import format_diff_as_json
-from gendiff.parser import parse
 
 
-def make_diff(data1, data2, parent=None):
-    """
-    Сравнивает два словаря и формирует результат.
+def parse(data, file_format):
+    if file_format == ".json":
+        return json.loads(data)
+    elif file_format in (".yml", ".yaml"):
+        return yaml.safe_load(data)
 
-    Args:
-        data1: первый словарь.
-        data2: второй словарь.
-        parent: родительский ключ.
 
-    Returns:
-        Результат сравнения в виде списка словарей.
-    """
-    if parent is None:
-        parent = []
+def get_data(file_path):
+    file_format = Path(file_path).suffix
+    with open(file_path) as f:
+        data = f.read()
+        return parse(data, file_format)
+
+
+def build_diff(file1, file2):
     diff = []
-    for key in data1.keys() | data2.keys():
-        path = [*parent, key]
-        path_str = '.'.join(str(p) for p in path)
-        if key not in data1:
-            diff.append({'path': path_str, 'status': 'added', 'value': data2[key]})
-        elif key not in data2:
-            diff.append({'path': path_str, 'status': 'deleted', 'value': data1[key]})
-        elif data1[key] != data2[key]:
-            diff.append({'path': path_str, 'status': 'updated', 'value': (data1[key], data2[key])})
-        elif isinstance(data1[key], (dict, list)) and isinstance(data2[key], (dict, list)):
-            diff.extend(make_diff(data1[key], data2[key], path))
+    for key in sorted(set(file1) | set(file2)):
+        if key not in file2:
+            diff.append({
+                "key": key,
+                "status": 'removed',
+                "value": file1[key],
+            })
+        elif key not in file1:
+            diff.append({
+                "key": key,
+                "status": 'added',
+                "value": file2[key]
+            })
+        elif isinstance(file1[key], dict) and isinstance(file2[key], dict):
+            nested_diff = build_diff(file1[key], file2[key])
+            if nested_diff:
+                diff.append({
+                    "key": key,
+                    "status": 'nested',
+                    "value": nested_diff
+                })
+        elif file1[key] == file2[key]:
+            diff.append({
+                "key": key,
+                "status": 'unchanged',
+                "value": file1[key]
+            })
         else:
-            diff.append({'path': path_str, 'status': 'unchanged', 'value': data1[key]})
+            diff.append({
+                "key": key,
+                "status": 'changed',
+                "old_value": file1[key],
+                "new_value": file2[key]
+            })
     return diff
 
 
-def generate_diff(filepath1, filepath2, output_format='stylish'):
-    filepath1_abs = os.path.abspath(filepath1)
-    filepath2_abs = os.path.abspath(filepath2)
-    diff = make_diff(dict(parse(filepath1_abs)), dict(parse(filepath2_abs)))
-    if output_format == 'stylish':
+def generate_diff(file_path1, file_path2, format_name='stylish'):
+    data1 = get_data(file_path1)
+    data2 = get_data(file_path2)
+    diff = build_diff(data1, data2)
+    if format_name == 'stylish':
         return format_diff_as_stylish(diff)
-    elif output_format == 'plain':
-        return format_diff_as_plain(diff).replace('\n\n', '\n').rstrip()
-    elif output_format == 'json':
+    elif format_name == "plain":
+        return format_diff_as_plain(diff)
+    elif format_name == 'json':
         return format_diff_as_json(diff)
-    else:
-        raise ValueError(f'Unknown format: {output_format}')
