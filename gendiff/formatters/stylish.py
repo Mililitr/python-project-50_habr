2780b2872b033ddc108b64491e355cec76044c2d
HardDuck69 <tmu1408@gmail.com>
2023-06-29 00:50:25 +0300

    upd stylish

--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -1,35 +1,67 @@
-import os
 from gendiff.parser import parse
 
 
-def build_indent(depth):
-    return " " * (depth * 4)
+class AddedDiff:
+    def __init__(self, key, value, depth):
+        self.key = key
+        self.value = value
+        self.depth = depth
+
+    def format(self):
+        return f"{build_indent(self.depth)}+ {self.key}: {format_value(self.value, self.depth)}"
+
+
+class RemovedDiff:
+    def __init__(self, key, value, depth):
+        self.key = key
+        self.value = value
+        self.depth = depth
+
+    def format(self):
+        return f"{build_indent(self.depth)}- {self.key}: {format_value(self.value, self.depth)}"
+
+
+class ChangedDiff:
+    def __init__(self, key, old_value, new_value, depth):
+        self.key = key
+        self.old_value = old_value
+        self.new_value = new_value
+        self.depth = depth
+
+    def format(self):
+        lines = []
+        lines.append(f"{build_indent(self.depth)}- {self.key}: {format_value(self.old_value, self.depth)}")
+        lines.append(f"{build_indent(self.depth)}+ {self.key}: {format_value(self.new_value, self.depth)}")
+        return "\n".join(lines)
+
+
+class UnchangedDiff:
+    def __init__(self, key, value, depth):
+        self.key = key
+        self.value = value
+        self.depth = depth
+
+    def format(self):
+        return f"{build_indent(self.depth)}  {self.key}: {format_value(self.value, self.depth)}"
+
+
+def format_dict(diff, depth=1):
+    if not isinstance(diff, dict):
+        return str(diff)
+    lines = []
+    for key, (status, value) in diff.items():
+        if status == "added":
+            diff_obj = AddedDiff(key, value, depth)
+        elif status == "removed":
+            diff_obj = RemovedDiff(key, value, depth)
+        elif status == "changed":
+            old_value, new_value = value
+            diff_obj = ChangedDiff(key, old_value, new_value, depth)
+        else:
+            diff_obj = UnchangedDiff(key, value, depth)
+        lines.append(diff_obj.format())
+    return "{\n" + "\n".join(lines) + "\n" + build_indent(depth - 1) + "}"
 
 
 def format_diff_as_stylish(diff):
-    def format_value(value, depth):
-        if isinstance(value, dict):
-            lines = []
-            for key, inner_value in value.items():
-                lines.append(f"{build_indent(depth + 1)}{key}: {format_value(inner_value, depth + 1)}")
-            return "{{\n" + "\n".join(lines) + f"\n{build_indent(depth)}}}"
-        else:
-            return str(value)
-
-    def format_dict(diff, depth=1):
-        if not isinstance(diff, dict):
-            return str(diff)
-        lines = []
-        for key, (status, value) in diff.items():
-            if status == "added":
-                lines.append(f"{build_indent(depth)}+ {key}: {format_value(value, depth)}")
-            elif status == "removed":
-                lines.append(f"{build_indent(depth)}- {key}: {format_value(value, depth)}")
-            elif status == "changed":
-                old_value, new_value = value
-                lines.append(f"{build_indent(depth)}- {key}: {format_value(old_value, depth)}")
-                lines.append(f"{build_indent(depth)}+ {key}: {format_value(new_value, depth)}")
-            else:
-                lines.append(f"{build_indent(depth)}  {key}: {format_value(value, depth)}")
-        return "{\n" + "\n".join(lines) + "\n" + build_indent(depth - 1) + "}"
     return format_dict(diff)
