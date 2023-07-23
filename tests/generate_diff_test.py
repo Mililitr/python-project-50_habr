ed9c36d421955111660a4ff7ab0c89583fbb52ee
HardDuck69 <tmu1408@gmail.com>
2023-07-23 23:21:55 +0300

    fixed tests

--- tests/generate_diff_test.py
+++ tests/generate_diff_test.py
@@ -15,29 +15,26 @@ def test_flat_diff_json():
     file_path2 = os.path.join(FIXTURES_PATH, 'file2.json')
     expected_result = json.loads(read_fixture(
         os.path.join(FIXTURES_PATH, 'json_result.txt')))
-    result = generate_diff(file_path1, file_path2, output_format='json')
-    assert sorted(result.splitlines()) == sorted(expected_result.splitlines())
-
+    result = generate_diff(file_path1, file_path2, format_name='json')
+    assert result == expected_result
 
 def test_flat_diff_plain():
     file_path1 = os.path.join(FIXTURES_PATH, 'file1.yml')
     file_path2 = os.path.join(FIXTURES_PATH, 'file2.yml')
     expected_result = read_fixture(os.path.join(FIXTURES_PATH, 'plain_result.txt')).strip() + '\n'
-    result = generate_diff(file_path1, file_path2, output_format='plain')
-    assert sorted(result.splitlines()) == sorted(expected_result.splitlines())
-
+    result = generate_diff(file_path1, file_path2, format_name='plain')
+    assert result == expected_result
 
 def test_nested_diff_plain():
     file_path1 = os.path.join(FIXTURES_PATH, 'nested/nested_file1.json')
     file_path2 = os.path.join(FIXTURES_PATH, 'nested/nested_file2.json')
     expected_result = read_fixture(os.path.join(FIXTURES_PATH, 'nested_result.txt'))
-    result = generate_diff(file_path1, file_path2, output_format='plain')
-    assert sorted(result.splitlines()) == sorted(expected_result.splitlines())
-
+    result = generate_diff(file_path1, file_path2, format_name='plain')
+    assert result == expected_result
 
 def test_stylish_diff_json():
     file_path1 = os.path.join(FIXTURES_PATH, 'file1.json')
     file_path2 = os.path.join(FIXTURES_PATH, 'file2.json')
     expected_result = read_fixture(os.path.join(FIXTURES_PATH, 'stylish_result.txt'))
-    result = generate_diff(file_path1, file_path2, output_format='stylish')
-    assert sorted(result.splitlines()) == sorted(expected_result.splitlines())
+    result = generate_diff(file_path1, file_path2, format_name='stylish')
+    assert result == expected_result
