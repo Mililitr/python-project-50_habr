83ab70bbd6a6b3703e55e81c94f1f863541da7bc
HardDuck69 <tmu1408@gmail.com>
2023-07-24 01:44:23 +0300

    make linter great again

--- gendiff/__init__.py
+++ gendiff/__init__.py
@@ -1,15 +1,4 @@
-import json
-from gendiff.formatters.stylish import format_diff_as_stylish
 from gendiff.generate_diff import generate_diff
 
 
-def format(data, formatter='stylish'):
-    if formatter == 'stylish':
-        return stylish.format(data)
-    elif formatter == 'json':
-        return json.dumps(data, indent=4)
-    else:
-        raise ValueError(f'Unknown formatter: {formatter}')
-
-
-__all__ = ['format']
+__all__ = ('generate_diff')
